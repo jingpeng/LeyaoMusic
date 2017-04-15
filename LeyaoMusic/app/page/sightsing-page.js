e20774a59b8c1996e4183f8876de5603295b37c8
王劲鹏 <jingpeng.wang@outlook.com>
2017-04-15 21:39:29 +0800

    完成视唱页UI

--- LeyaoMusic/app/page/sightsing-page.js
+++ LeyaoMusic/app/page/sightsing-page.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import {
-  Image
+  Dimensions,
+  Image,
+  Text,
+  View
 } from 'react-native';
 
 export default class SightsingPage extends Component {
@@ -11,12 +14,84 @@ export default class SightsingPage extends Component {
         source={ require('../resource/main-background.jpg') }
         style={{
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
           width: null,
           height: null,
           backgroundColor: 'rgba(0, 0, 0, 0)',
         }}>
+        <View
+          style={{
+            marginTop: 20,
+            height: 44,
+            alignSelf: 'center',
+            justifyContent: 'center'
+          }}>
+          <Text
+            style={{
+              fontFamily: 'ArialMT',
+              fontSize: 18,
+              color: '#ffffff'
+            }}>视唱</Text>
+        </View>
+        <View
+          style={{
+            width: Dimensions.get('window').width,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: 10
+          }}>
+          <Image
+            source={ require('../resource/stave.png') }
+            style={{
+              width: 20.5,
+              height: 14.5,
+              marginLeft: 10
+            }}/>
+          <Text
+            style={{
+              fontFamily: 'ArialMT',
+              fontSize: 13,
+              color: '#ffffff',
+              marginLeft: 15
+            }}>五线谱介绍，谱号，加线</Text>
+        </View>
+        <Image
+          source={ require('../resource/sightsing-divider.png') }
+          style={{
+            width: Dimensions.get('window').width,
+            height: 1,
+            marginTop: 11,
+            marginLeft: 11
+          }}/>
+        <View
+          style={{
+            width: Dimensions.get('window').width,
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginTop: 14
+          }}>
+          <Image
+            source={ require('../resource/note-duration.png') }
+            style={{
+              width: 18.5,
+              height: 16,
+              marginLeft: 10
+            }}/>
+          <Text
+            style={{
+              fontFamily: 'ArialMT',
+              fontSize: 13,
+              color: '#ffffff',
+              marginLeft: 15
+            }}>音符时值（持续时间长短）</Text>
+        </View>
+        <Image
+          source={ require('../resource/sightsing-divider.png') }
+          style={{
+            width: Dimensions.get('window').width,
+            height: 1,
+            marginTop: 11,
+            marginLeft: 11
+          }}/>
       </Image>
     );
   }
