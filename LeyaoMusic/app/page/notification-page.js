a9f785bd3c84d4fe8a1c3462733c5b70ad05b3cd
王劲鹏 <jingpeng.wang@outlook.com>
2017-04-16 14:18:47 +0800

    完成消息页面列表

--- LeyaoMusic/app/page/notification-page.js
+++ LeyaoMusic/app/page/notification-page.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {
   Dimensions,
   Image,
+  ListView,
   Text,
   TouchableWithoutFeedback,
   View
@@ -11,6 +12,14 @@ import {
 } from 'react-native-router-flux';
 
 export default class NotificationPage extends Component {
+
+  constructor(props) {
+    super(props)
+    var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    this.state = {
+      dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+    }
+  }
 
   back() {
     Actions.pop()
@@ -66,6 +75,61 @@ export default class NotificationPage extends Component {
             </View>
           </TouchableWithoutFeedback>
         </View>
+        <ListView
+          dataSource={ this.state.dataSource }
+          renderRow={ (rowData) =>
+            <View
+              style={{
+                width: Dimensions.get('window').width,
+                marginTop: 20,
+                flexDirection: 'row',
+                justifyContent: 'center'
+              }}>
+              <View
+                style={{
+                  width: Dimensions.get('window').width - 46,
+                  borderRadius: 5,
+                  backgroundColor: 'rgba(0, 0, 0, 0.5)'
+                }}>
+                <Text
+                  style={{
+                    fontFamily: 'ArialMT',
+                    fontSize: 15,
+                    marginLeft: 35,
+                    marginTop: 10,
+                    color: '#ffffff'
+                  }}>系统消息</Text>
+                <Text
+                  style={{
+                    fontFamily: 'ArialMT',
+                    fontSize: 13,
+                    marginLeft: 35,
+                    marginRight: 35,
+                    marginTop: 5,
+                    marginBottom: 10,
+                    color: 'rgba(255, 255, 255, 0.8)'
+                  }}>系统消息系统消息系统消息系统消息系统消息系统消息系统消息系统消息系统消息系统消息系统消息系统消息</Text>
+                <Text
+                  style={{
+                    position: 'absolute',
+                    fontFamily: 'ArialMT',
+                    fontSize: 10,
+                    right: 10,
+                    top: 10,
+                    color: 'rgba(255, 255, 255, 0.6)'
+                  }}>2017-3-4  13:00</Text>
+              </View>
+              <Image
+                source={ require('../resource/music-note-set.jpg') }
+                style={{
+                  width: 27.5,
+                  height: 37.5,
+                  top: -10,
+                  left: 18,
+                  position: 'absolute'
+                }}/>
+            </View>
+          }/>
       </Image>
     );
   }
