acfadc0656319396363a75dcbd98e4cf8a159d10
王劲鹏 <jingpeng.wang@outlook.com>
2017-06-01 08:58:25 +0800

    修改练耳页，修改接口base地址

--- LeyaoMusic/app/page/ear-training-page.js
+++ LeyaoMusic/app/page/ear-training-page.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
 import {
-  Image
+  Dimensions,
+  Image,
+  Text,
+  TouchableWithoutFeedback,
+  View
 } from 'react-native';
+import {
+  Actions
+} from 'react-native-router-flux';
 
 export default class EarTrainingPage extends Component {
+
+  staveIntroduce() {
+    Actions.stave_introduction()
+  }
+
+  noteDuration() {
+    Actions.note_duration()
+  }
 
   render() {
     return (
@@ -11,12 +26,90 @@ export default class EarTrainingPage extends Component {
         source={ require('../resource/main-background.jpg') }
         style={{
           flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
           width: null,
           height: null,
           backgroundColor: 'rgba(0, 0, 0, 0)',
         }}>
+        <View
+          style={{
+            marginTop: 20,
+            height: 44,
+            alignSelf: 'center',
+            justifyContent: 'center'
+          }}>
+          <Text
+            style={{
+              fontFamily: 'ArialMT',
+              fontSize: 18,
+              color: '#ffffff'
+            }}>练耳</Text>
+        </View>
+        <TouchableWithoutFeedback
+          onPress={ this.staveIntroduce.bind(this) }>
+          <View
+            style={{
+              width: Dimensions.get('window').width,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 10
+            }}>
+            <Image
+              source={ require('../resource/stave.png') }
+              style={{
+                width: 20.5,
+                height: 14.5,
+                marginLeft: 10
+              }}/>
+            <Text
+              style={{
+                fontFamily: 'ArialMT',
+                fontSize: 13,
+                color: '#ffffff',
+                marginLeft: 15
+              }}>五线谱介绍，谱号，加线</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <Image
+          source={ require('../resource/sightsing-divider.png') }
+          style={{
+            width: Dimensions.get('window').width,
+            height: 1,
+            marginTop: 11,
+            marginLeft: 11
+          }}/>
+        <TouchableWithoutFeedback
+          onPress={ this.noteDuration.bind(this) }>
+          <View
+            style={{
+              width: Dimensions.get('window').width,
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 14
+            }}>
+            <Image
+              source={ require('../resource/note-duration.png') }
+              style={{
+                width: 18.5,
+                height: 16,
+                marginLeft: 10
+              }}/>
+            <Text
+              style={{
+                fontFamily: 'ArialMT',
+                fontSize: 13,
+                color: '#ffffff',
+                marginLeft: 15
+              }}>音符时值（持续时间长短）</Text>
+          </View>
+        </TouchableWithoutFeedback>
+        <Image
+          source={ require('../resource/sightsing-divider.png') }
+          style={{
+            width: Dimensions.get('window').width,
+            height: 1,
+            marginTop: 11,
+            marginLeft: 11
+          }}/>
       </Image>
     );
   }
--- LeyaoMusic/app/service/api-constant.js
+++ LeyaoMusic/app/service/api-constant.js
@@ -1,6 +1,9 @@
 export default class APIConstant {
-  static BASE_URL = "http://123.56.220.72:8080/poker/api"
-  static BASE_FILE_URI = "http://123.56.220.72:8080/poker/"
+  //static BASE_URL = "http://123.56.220.72:8080/poker/api"
+  //static BASE_FILE_URI = "http://123.56.220.72:8080/poker/"
+
+  static BASE_URL = "http://panruifeng.loan/poker/api"
+  static BASE_FILE_URI = "http://panruifeng.loan/poker/"
 
   static STATUS_SUCCEED = "SUCCEED"
   static STATUS_FAILED = "FAILED"
